2c8d508c013db1d13dd8dd1b85c8dd20662668af
Hwang-seo-i <hnvvely@gmail.com>
2023-02-10 11:38:55 +0900

    0210

--- Front_Day03/0210/src/App.js
+++ Front_Day03/0210/src/App.js
@@ -1,11 +1,27 @@
-import { useEffect } from "react";
-import 
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [saramList, setSaramList] = useState([]);
+  useEffect(() => {
+    // axios 모듈을 활용한 Ajax 처리
+    axios.get("http://localhost:5000/car").then((response) => {
+      setSaramList(response.data);
+    });
+  });
 
   return (
     <div>
-      <h1>서이의 홈페이지</h1>
+      <h1>길동이의 홈페이지</h1>
+      <ul>
+        {saramList.map((saram) => {
+          return (
+            <li key={saram.no}>
+              {saram.name}, {saram.company}, {saram.year}
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
